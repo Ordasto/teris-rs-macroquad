c366c1b10fd34201903414b5565ed5da098dcf10
Ordo <sam.peters24@gmail.com>
2023-11-08 20:01:55 +0000

    confusion

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use macroquad::{prelude::*, rand};
+use tetriminos::*;
 
 #[macroquad::main("tetris")]
 async fn main() {
@@ -49,20 +50,33 @@ async fn main() {
 }
 
 // Could have a color overwrite Option<Color> Some() or tet.1
-fn draw_tetrimino(tet: &TetriminoType, x: i32, y: i32) {
-    for i in 0..16 {
-        if tet.0 as usize & 0b_1000_0000_0000_0000 >> i != 0 {
-            let x = (i % 4) + 1 + x; // + 1 to both to avoid zero indexing
-            let y = (i / 4) + 1 + y;
+// fn draw_tetrimino(tet: &TetriminoType, x: i32, y: i32) {
+//     for i in 0..16 {
+//         if tet.0 as usize & 0b_1000_0000_0000_0000 >> i != 0 {
+//             let x = (i % 4) + 1 + x; // + 1 to both to avoid zero indexing
+//             let y = (i / 4) + 1 + y;
+//
+//             // this is probably pretty inefficient
+//             draw_rectangle(
+//                 BOARD_POS.x * x as f32,
+//                 BOARD_POS.y * y as f32,
+//                 10.0,
+//                 10.0,
+//                 tet.1,
+//             );
+//         }
+//     }
+// }
 
-            // this is probably pretty inefficient
-            draw_rectangle(
-                BOARD_POS.x * x as f32,
-                BOARD_POS.y * y as f32,
-                10.0,
-                10.0,
-                tet.1,
-            );
+// will probably need to draw each square seperatly as they need to disapear seperatly
+// further version could use textures with larger destinations sizes and move the active
+// tet into a board array when it hits something,
+fn draw_tetrimino(tet: &TetriminoType, x: i32, y: i32, rot: i32) {
+    let x = x as f32; // dont do this, just change the param
+    let y = y as f32;
+    for i in tet.0 {
+        for j in i {
+            draw_rectangle(x, y, 10.0, 10.0, tet.1);
         }
     }
 }
@@ -70,20 +84,59 @@ fn draw_tetrimino(tet: &TetriminoType, x: i32, y: i32) {
 // where the board starts
 const BOARD_POS: Vec2 = vec2(10.0, 10.0);
 
-struct TetriminoType([[i32; 4]; 4], Color);
-
-const TETRIMINO_I: [[i32; 4]; 4] = [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]];
-
-const TETRIMINO_J: [[i32; 3]; 3] = [[0, 0, 1], [1, 1, 1], [0, 0, 0]];
-
-const TETRIMINO_L: [[i32; 3]; 3] = [[1, 0, 0], [1, 1, 1], [0, 0, 0]];
-
-const TETRIMINO_O: [[i32; 2]; 2] = [[1, 1], [1, 1]];
-
-const TETRIMINO_S: [[i32; 3]; 3] = [[0, 1, 1], [1, 1, 0], [0, 0, 0]];
-
-const TETRIMINO_T: [[i32; 3]; 3] = [[0, 1, 0], [1, 1, 1], [0, 0, 0]];
-
-const TETRIMINO_Z: [[i32; 3]; 3] = [[1, 1, 0], [0, 1, 1], [0, 0, 0]];
-
-const CYAN: Color = color_u8!(0, 255, 255, 255); // Guess ill do it myself
+#[rustfmt::skip]
+mod tetriminos {
+    use macroquad::{color::*, color_u8};
+    
+    struct TetriminoType([[i32; 4]; 4], Color);
+    const CYAN: Color = color_u8!(0, 255, 255, 255); // Guess ill do it myself
+    
+    const TETRIMINO_I: TetriminoType = ([
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ],CYAN);
+    
+    const TETRIMINO_J: TetriminoType = ([
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],BLUE);
+    
+    const TETRIMINO_L: TetriminoType = ([
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],ORANGE);
+    
+    const TETRIMINO_O: TetriminoType = ([
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],YELLOW);
+    
+    const TETRIMINO_S: TetriminoType = ([
+        [0, 1, 1, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+    ],GREEN);
+    
+    const TETRIMINO_T: TetriminoType = ([
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+    ],PURPLE);
+    
+    const TETRIMINO_Z: TetriminoType = ([
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 0],
+    ],RED);
+}
